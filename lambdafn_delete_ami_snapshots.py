fc9e374feed64be0b9c973f7448ca6213e4336a5
Mahendra M <89565913+creazer-I@users.noreply.github.com>
2023-02-09 11:29:07 +0530

    Update lambdafn_delete_ami_snapshots.py

--- lambdafn_delete_ami_snapshots.py
+++ lambdafn_delete_ami_snapshots.py
@@ -1,3 +1,4 @@
+# These are the libraries that are being imported.
 import json
 import boto3
 import botocore
@@ -6,6 +7,13 @@ import time
 
 
 def describeami(client,inputDate):
+    """
+    It takes the client and the input date as parameters and then it checks if the input date is present
+    in the creation date of the AMI. If it is present, it deregisters the AMI
+    
+    :param client: The boto3 client object
+    :param inputDate: The date you want to delete AMIs from
+    """
     amidescribe = client.describe_images(
     Owners=[
         '811038047831',
@@ -34,6 +42,12 @@ def describeami(client,inputDate):
             print("Ami not present in year :",inputDate)
         
 def describesnapshot(client,inputDate):
+    """
+    It takes a client object and a date as input and deletes all snapshots created in that year
+    
+    :param client: The boto3 client object
+    :param inputDate: The year for which you want to delete the snapshots
+    """
     snapshotdescribe = client.describe_snapshots(
         
         OwnerIds=[
@@ -71,6 +85,14 @@ def describesnapshot(client,inputDate):
     
 
 def lambda_handler(event, context):
+    """
+    It takes the input date, and then calls the describeami and describesnapshot functions, passing the
+    input date as a parameter.
+    
+    :param event: This is the event that triggered the lambda function
+    :param context: It's a Lambda-specific object that contains information about the invocation,
+    function, and execution environment
+    """
     
     inputDate = '2019'
     
@@ -79,15 +101,3 @@ def lambda_handler(event, context):
     describeami(client,inputDate)
     
     describesnapshot(client,inputDate)
-    
-    
-    
-    
-
-    
-            
-
-    
-
-        
-        
