5e495ce692c58b6715d6ac8efde5cde70b76844d
Mahendra M <89565913+creazer-I@users.noreply.github.com>
2023-08-28 17:56:49 +0530

    Update terraformrd_proxy.tf

--- terraformrd_proxy.tf
+++ terraformrd_proxy.tf
@@ -27,17 +27,14 @@ resource "aws_db_proxy_default_target_group" "this" {
   db_proxy_name = aws_db_proxy.db_proxy.name
 
   dynamic "connection_pool_config" {
-    for_each = (
-      var.connection_borrow_timeout != null || var.init_query != null || var.max_connections_percent != null ||
-      var.max_idle_connections_percent != null || var.session_pinning_filters != null
-    ) ? ["true"] : []
+    for_each = var.connection_pool_config
 
     content {
-      connection_borrow_timeout    = var.connection_borrow_timeout
-      init_query                   = var.init_query
-      max_connections_percent      = var.max_connections_percent
-      max_idle_connections_percent = var.max_idle_connections_percent
-      session_pinning_filters      = var.session_pinning_filters
+      connection_borrow_timeout    = connection_pool_config.value["connection_borrow_timeout"] == "" ? null : connection_pool_config.value["connection_borrow_timeout"]
+      init_query                   = connection_pool_config.value["init_query"] == "" ? null : connection_pool_config.value["init_query"]
+      max_connections_percent      = connection_pool_config.value["max_connections_percent"] == "" ? null : connection_pool_config.value["max_connections_percent"]
+      max_idle_connections_percent = connection_pool_config.value["max_idle_connections_percent"] == "" ? null : connection_pool_config.value["max_idle_connections_percent"]
+      session_pinning_filters      = connection_pool_config.value["session_pinning_filters"] == "" ? null : connection_pool_config.value["session_pinning_filters"]
     }
   }
 }
@@ -70,6 +67,9 @@ resource "aws_cloudwatch_log_group" "this" {
   skip_destroy      = false
   tags = var.tags
 }
+
+
+
 
 
 
